44b1d046a9b396009e25664f07876c4996ec4093
leedo97y <dlehdus1433@gmail.com>
2024-10-30 23:39:47 +0900

    feat: #11 header에 세계지도 페이지로 갈 수 있는 버튼 추가, 메인 페이지에 툴팁 추가

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,22 @@
-import { HeaderContainer, HomeTitle } from "@/styles/HeaderStyle";
+import { HeaderContainer, HomeTitleSection, WorldMapSection } from "@/styles/HeaderStyle";
 import Link from "next/link";
+import { PiMapTrifold } from "react-icons/pi";
 
 const Header = () => {
   return (
     <HeaderContainer>
-      <HomeTitle>
-        <Link href={"/"}>
+      <HomeTitleSection>
+        <Link href={"/"} title="home page">
           <span id="title">내</span> <span id="title">식</span>
           <span>물을</span> <span id="title">소</span>
           <span>개합니다</span>
         </Link>
-      </HomeTitle>
+      </HomeTitleSection>
+      <WorldMapSection>
+        <Link href={"/world"} title="world map page">
+          <PiMapTrifold />
+        </Link>
+      </WorldMapSection>
     </HeaderContainer>
   );
 };
